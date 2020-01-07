b1ca7113cde7e3e52fe402dc986d4a605964e37d
Guy Roger Eboulet <eboulet87@gmail.com>
2020-01-07 15:32:23 +0100

    Mail - inserimento utenti

---
+++ mail/script_mail.js
@@ -0,0 +1,10 @@
+var nomeUtente = parseInt(prompt("Scrivi la tua mail?"));
+var eMail = ["rosso@gmail.com","giallo@gmail.com","verde@gmail.com","blu@gmail.com","viola@gmail.com"];
+for (var i = 0; i < eMail.length; i++) {
+  var mail = console.log(eMail[i]);
+
+  if (true) {
+
+  }
+
+}
