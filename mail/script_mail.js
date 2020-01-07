6fe8bc32f1966325d4c73cc757f0dc2a235c529c
Guy Roger Eboulet <eboulet87@gmail.com>
2020-01-07 16:19:06 +0100

    Mail - commit finale

--- mail/script_mail.js
+++ mail/script_mail.js
@@ -1,10 +1,16 @@
-var nomeUtente = parseInt(prompt("Scrivi la tua mail?"));
+var nomeUtente = prompt("Scrivi la tua mail?");
 var eMail = ["rosso@gmail.com","giallo@gmail.com","verde@gmail.com","blu@gmail.com","viola@gmail.com"];
+var trovato = false;
+
 for (var i = 0; i < eMail.length; i++) {
-  var mail = console.log(eMail[i]);
+  var mail = eMail[i];
 
-  if (true) {
-
+  if (nomeUtente==mail) {
+    trovato = true;
+    alert("Utente riconosciuto")
   }
 
+  else if (nomeUtente!=mail)
+    trovato = false;
+    alert("Utente non riconosciuto, riprova!!")
 }
